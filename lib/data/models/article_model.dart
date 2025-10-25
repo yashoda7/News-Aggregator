acd8cb7e8811432084620dd180d9e912d1d6c511
yashoda7 <gundayashoda7@gmail.com>
2025-10-25 16:14:33 +0530

    feature:added required dependices and implement the article entity and model

---
+++ lib/data/models/article_model.dart
@@ -0,0 +1,43 @@
+import 'package:json_annotation/json_annotation.dart';
+import '../../domain/entities/article.dart';
+
+part 'article_model.g.dart';
+
+@JsonSerializable()
+class ArticleModel {
+  final Map<String, dynamic>? source;
+  final String? author;
+  final String title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final String? publishedAt;
+  final String? content;
+
+  ArticleModel({
+    this.source,
+    this.author,
+    required this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) =>
+      _$ArticleModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ArticleModelToJson(this);
+
+  Article toEntity() => Article(
+        author: author,
+        title: title,
+        description: description,
+        url: url,
+        urlToImage: urlToImage,
+        publishedAt: publishedAt != null ? DateTime.tryParse(publishedAt!) : null,
+        content: content,
+        sourceName: source?['name'] as String?,
+      );
+}
---
+++ lib/domain/entities/article.dart
@@ -0,0 +1,21 @@
+class Article {
+  final String? author;
+  final String title;
+  final String? description;
+  final String? url;
+  final String? urlToImage;
+  final DateTime? publishedAt;
+  final String? content;
+  final String? sourceName;
+
+  Article({
+    this.author,
+    required this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+    this.sourceName,
+  });
+}
